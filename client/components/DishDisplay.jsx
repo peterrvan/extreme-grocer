9593c8cef7ab5cc44c6495408f0ed8a133ab0009
Peter Van <peterrvan@gmail.com>
2021-05-20 17:25:20 -0700

    Finalized minimum viable product for presentation

--- client/components/DishDisplay.jsx
+++ client/components/DishDisplay.jsx
@@ -26,7 +26,7 @@
   return(
     <div className="dishDisplay">
         <div key={props.index}>
-            <h1>{props.title}</h1>
+            <h3>{props.title}</h3>
             <img src={props.image}></img>
         </div>
         <div className="flex">
